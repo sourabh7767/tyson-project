4b1bbc5f47e4a34c6697ab6168964536983c2d61
Sourabh <saurav7767@gmail.com>
2023-09-05 21:32:16 +0530

    updates

--- resources/views/booking/slot-number.blade.php
+++ resources/views/booking/slot-number.blade.php
@@ -1,3 +1,3 @@
-<label class="form-label" for="no_of_slots">No Of Sloats 
+<label class="form-label" for="no_of_slots">No Of Sloats Available
     <span class="text-danger asteric-sign">&#42;</span></label>
     <input type = "text"  class="form-control" readonly value={{$slotsData->remaining_slots}}>
