49ce67e81b818cc5364025fa1e580fbbad223c8b
Sourabh <saurav7767@gmail.com>
2023-09-13 22:26:17 +0530

    updates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -49,8 +49,9 @@ class UserController extends Controller
                 $btn = '';
                 $btn = '<a href="' . route('users.show',encrypt($user->id)) . '" title="View"><i class="fas fa-eye"></i></a>&nbsp;&nbsp;';
                 $btn .= '<a href="' . route('users.edit',encrypt($user->id)) . '" title="Edit"><i class="fas fa-edit"></i></a>&nbsp;&nbsp;';
-                $btn .= '<a href="' . route('user.changeUserPassword',encrypt($user->id)) . '" title="Change Password">Change password</a>&nbsp;&nbsp;';
+                
                 $btn .= '<a href="javascript:void(0);" delete_form="delete_customer_form"  data-id="' .$user->id. '" class="delete-datatable-record text-danger delete-users-record" title="Delete"><i class="fas fa-trash"></i></a>';
+                $btn .= '<a href="' . route('user.changeUserPassword',encrypt($user->id)) . '" title="Change Password">Change password</a>&nbsp;&nbsp;';
 
                 return $btn;
             })
