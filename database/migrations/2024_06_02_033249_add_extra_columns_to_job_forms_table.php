33f86841e524d40fbf3d259312fe033c555f4cbb
Laksh_tech <lakshdeep@itechnolabs.tech>
2024-06-02 15:53:44 +0530

    revert migration

--- database/migrations/2024_06_02_033249_add_extra_columns_to_job_forms_table.php
+++ database/migrations/2024_06_02_033249_add_extra_columns_to_job_forms_table.php
@@ -16,7 +16,7 @@ class AddExtraColumnsToJobFormsTable extends Migration
         Schema::table('job_forms', function (Blueprint $table) {
             $table->tinyInteger('is_lead')->default(0)->comment('0 => NO, 1 => YES');
             $table->double('admin_comission_per')->nullable();
-            $table->double('admin_comission_amount_per_per')->nullable();
+            $table->double('admin_comission_amount_per')->nullable();
         });
     }
 
@@ -30,7 +30,7 @@ class AddExtraColumnsToJobFormsTable extends Migration
         Schema::table('job_forms', function (Blueprint $table) {
             $table->dropColumn('status');
             $table->dropColumn('admin_comission_per');
-            $table->dropColumn('admin_comission_amount_per_per');
+            $table->dropColumn('admin_comission_amount_per');
         });
     }
 }
