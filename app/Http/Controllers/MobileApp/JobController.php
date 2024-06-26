b6e23e4263d181c0048aeec9012e8caf5d7be51f
Sourabh <saurav7767@gmail.com>
2024-06-26 23:34:42 +0530

    updares

--- app/Http/Controllers/MobileApp/JobController.php
+++ app/Http/Controllers/MobileApp/JobController.php
@@ -220,7 +220,6 @@ public function update(Request $request){
                 // $this->sendPushNotification($jobObj->user->fcm_token,"Admin Comment","Admin Commented On your job",$jobObj->user->device_type,$data,0);
                 if(!empty($jobObj->user->fcm_token)){
                     $configResult  = $this->fireBaseConfig();
-                    echo "<pre>";print_r($configResult);die;
                     $this->sendFireBasePushNotification($configResult->access_token,$jobObj->user->fcm_token,"Comment",$request->comment,$jobObj->id);
                 }
             session()->flash('success',"Job Updated");
